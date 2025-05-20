b4782a74bb08179dd1c59d9a0a8fbce2d29a43c4
SponsoredByPuma <tillwanner01@gmail.com>
2025-05-20 16:11:52 +0200

    feat: add component library

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
 	import '../app.css';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
+	import Paperclip from '@lucide/svelte/icons/paperclip';
+	import Calendar from '@lucide/svelte/icons/calendar';
+	import CircleUser from '@lucide/svelte/icons/circle-user';
+	import Menu from '@lucide/svelte/icons/menu';
 
 	let { children } = $props();
 </script>
 
 
 <div class="min-h-screen flex flex-col bg-green-200">
+	<AppBar headlineClasses="sm:hidden" centerClasses="hidden sm:block">
+  {#snippet lead()}
+  <ArrowLeft/>
+  {/snippet}
+  {#snippet trail()}
+    <div class="hidden space-x-4 sm:flex">
+      <Paperclip size={20} />
+      <Calendar size={20} />
+      <CircleUser size={20} />
+    </div>
+    <div class="block sm:hidden">
+      <Menu size={20} />
+    </div>
+  {/snippet}
+  {#snippet headline()}
+    <h2 class="h2">Step Fish</h2>
+  {/snippet}
+  <span>Step Fish</span>
+</AppBar>
 {@render children()}
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <div class="size-full">
+    <navbar>
+        <button on:click={handleClick}> Go to Data Privacy Policy</button>
+    </navbar>
     <div class="m-4">
         <h1 class="text-3xl font-bold text-blue-600 ">Welcome to SvelteKit</h1>
         <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-        <button on:click={handleClick}> test me </button>
+        <button type="button" class="btn preset-filled-primary-500">Button</button>
     </div>
 </div>
 
