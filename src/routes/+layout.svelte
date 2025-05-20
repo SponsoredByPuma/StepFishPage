3e3fcedb4aed3ec6b96cb755ad863651a42974e4
SponsoredByPuma <tillwanner01@gmail.com>
2025-05-20 15:34:21 +0200

    Replace Vue project with new Svelte project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+
+<div class="min-h-screen flex flex-col bg-green-200">
+{@render children()}
+</div>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="size-full">
+    <div class="m-4">
+        <h1 class="text-3xl font-bold text-blue-600 ">Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+        <button on:click={handleClick}> test me </button>
+    </div>
+</div>
+
+<script lang="ts">
+  import { goto } from '$app/navigation';
+
+  function handleClick() {
+    goto('/dataprivacypolicy');
+  }
+</script>
