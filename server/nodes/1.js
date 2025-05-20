694661c409dc7830df3699fe5753a08c17316171
SponsoredByPuma <tillwanner01@gmail.com>
2025-05-20 16:06:59 +0200

    Updates

--- server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DHIIDn0J.js","_app/immutable/chunks/DKnHX_Ip.js","_app/immutable/chunks/B7raxhyI.js","_app/immutable/chunks/BoqpPzDE.js","_app/immutable/chunks/Bu4eQ944.js","_app/immutable/chunks/BLcp3-99.js","_app/immutable/chunks/B26qZVl3.js","_app/immutable/chunks/B9A-4HTs.js","_app/immutable/chunks/BPv4eLHE.js","_app/immutable/chunks/BnI5fpHY.js"];
-export const stylesheets = [];
-export const fonts = [];
